493b5ee1e33a8f820ce5c7d03d06f90cf9851dfe
wsaebo <wsaebo@gmail.com>
2024-02-26 16:39:58 +0100

    Create liste.py

---
+++ liste.py
@@ -0,0 +1,5 @@
+liste=[10,60,65] #Legger til tallene 10, 60 og 65 i listen
+liste.append(6) #Legger til tallet 6 i lista
+print ("Første element", liste[0] ,"og trejde element",liste[2]) #Printer ut element 1 som er 10 og element 3 som er 65
+
+
