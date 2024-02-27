e14fc6a43b0d3640ef463b8960c27f0061dcfe06
wsaebo <wsaebo@gmail.com>
2024-02-27 12:19:51 +0100

    Nye commit

--- liste.py
+++ liste.py
@@ -1,5 +1,5 @@
 #Oppgave 1 om liste 
-import numpy 
+import numpy #Importeren modulen numpy for å kunne få tilgang til matematiske funksjoner. 
 liste=[10,60,65] #Legger til tallene 10, 60 og 65 i listen
 liste.append(6) #Legger til tallet 6 i lista
 print ("Første element", liste[0] ,"og trejde element",liste[2]) #Printer ut element 1 som er 10 og element 3 som er 65
---
+++ ordbok.py
@@ -0,0 +1,3 @@
+#oppgave 2 om ordbok
+ordbok={"melk":14.90}
+print (ordbok)
