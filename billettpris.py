5d14a61aa114b802ed24715e9fb86bd0a2e418d7
wsaebo <wsaebo@gmail.com>
2024-02-27 15:12:15 +0100

    Update billettpris.py

--- billettpris.py
+++ billettpris.py
@@ -3,7 +3,16 @@
 def billettpris():
   print("Skriv alder på kjøperen")
   alder=input(int)
-  print(alder)
+  billettpris=0
+  if alder <=17:
+    billettpris=30 
+  elif alder >17&alder<=67:
+    bilettpris=50
+  else:
+    billettpris=35
+
+    print("Billettprisen er ", billettpris)
+
 
 
 billettpris()
